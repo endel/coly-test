99887960682a8225ed9b946ec2204fa4733ed8f6
Mykybo <Mykybo@users.noreply.github.com>
2019-04-16 09:23:58 +0200

    Update TestRoom.ts

--- src/TestRoom.ts
+++ src/TestRoom.ts
@@ -3,18 +3,16 @@ import { Room } from 'colyseus'
 
 class State extends Schema {
   @type('string')
-  lastMessage: string = ''
+  someAtr: string = ''
 }
 
 export class TestRoom extends Room<State> {
-  maxClients = 4
-
-  onInit(options) {
-    console.log('chat on init')
+  onInit() {
+    console.log('on init')
     this.setState(new State())
   }
 
-  onJoin(client, options, auth) {
+  onJoin() {
     console.log('client has joined!')
   }
 
